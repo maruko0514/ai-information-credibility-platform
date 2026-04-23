059e1942e63ad6ecd48d4569eeb46a56956170ea
maruko0514 <marukowwt@gmail.com>
2026-04-23 05:12:30 +0000

    add index.html

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Docker Flask!"
+    return """<html>
+            <head>
+                <title>首頁</title>
+            </head>
+            <body>
+                <h1>歡迎來到我的網站</h1>
+                <p>這是用 Flask 直接回傳 HTML</p>
+
+                <a href="/about">前往 About</a>
+            </body>
+        </html>"""
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+        host="0.0.0.0", 
+        port=int(os.environ.get("PORT",5000)),
+        debug=True
